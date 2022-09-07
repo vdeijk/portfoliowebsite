bb1b739564a445360b79a7dcbafc69ff074be664
Rick van der Eijk <vandereijkwebdevelopment@gmail.com>
2022-09-07 13:13:29 +0200

    final changes

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,12 +1,21 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-homepage",
-  templateUrl: "./homepage.component.html",
-  styleUrls: ["./homepage.component.scss"],
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss'],
 })
 export class HomepageComponent implements OnInit {
   constructor() {}
 
+  displayFullText = false;
+  aboutButtonText = 'Read more';
+
   ngOnInit(): void {}
+
+  expandText() {
+    this.displayFullText = !this.displayFullText;
+    if (!this.displayFullText) this.aboutButtonText = 'Read more';
+    else this.aboutButtonText = 'Read less';
+  }
 }
