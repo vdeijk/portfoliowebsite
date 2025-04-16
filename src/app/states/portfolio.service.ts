e4226c60ff625035341cd3977e045c5c39026b18
Rick van der Eijk <vandereijkwebdevelopment@gmail.com>
2025-04-16 16:59:18 +0200

    portfolio updates

--- src/app/states/portfolio.service.ts
+++ src/app/states/portfolio.service.ts
@@ -6,16 +6,22 @@ import { CardLinks } from '../interfaces/cardLink';
 })
 export class PortfolioService {
   cardsTitles: string[] = [
+    'GrowHub Frontend',
+    'GrowHub Backend',
     'Mech Commander',
     'Portfolio',
   ];
 
   cardsFeatures: string[][] = [
+    ['React', 'MoxB', 'UI/UX', 'Maintainble code'],
+    ['C#', '.NET', 'Swagger', 'Maintainble code'],
     ['Unity', '80+ C# Scripts', 'UI/UX', 'Vector Math'],
     ['Angular', 'Adv. CSS', 'UI/UX', 'Maintable code'],
   ];
 
   cardsLinks: CardLinks[] = [
+    { view: '', code: 'https://github.com/vdeijk/GrowHubFrontend' },
+    { view: '', code: 'https://github.com/vdeijk/GrowHubBackend' },
     { view: 'https://vdeijk.itch.io/mech-commander', code: '' },
     {
       view: '',
